5c72c9dfdd7526598397f55bc62fa322903f81a6
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-02-12 08:50:08 +0700

    update rehan wangsapp

--- RehanWangsappApp/app/src/main/java/com/rmaproject/rehanwangsappapp/SplashScreenActivity.kt
+++ RehanWangsappApp/app/src/main/java/com/rmaproject/rehanwangsappapp/SplashScreenActivity.kt
@@ -12,6 +12,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }, 3000)
     }
 }
